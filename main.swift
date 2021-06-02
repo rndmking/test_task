060c0b4763e4d470626e58e28d8963513e3b209c
rndmking <31206554+rndmking@users.noreply.github.com>
2021-06-02 21:08:36 +0300

    Minor codestyle fix

--- main.swift
+++ main.swift
@@ -1,24 +1,17 @@
-//
-//  main.swift
-//  test
-//
-//  Created by r.gadeev on 02.06.2021.
-//
-
 import Foundation
 
 var i: Int = 1
 var input = readLine() ?? ""
-// var array5: String = input.unicodeScalars.filter { !$0.properties.isEmojiPresentation }.reduce(""){$0  + String($1)}
-var array4: [String] = input.components(separatedBy: " ")
-var array: [Int?] = array4.map {Int($0)}
-var min: Int = Int.max
+var arrayOfStrings: [String] = input.components(separatedBy: " ")
+var array: [Int?] = arrayOfStrings.map { Int($0) }
+var min = Int.max
 var index: Int
 i = 1
-array.enumerated().forEach( {index, value in
-    if value ?? Int.max < min && value != nil {
+array.enumerated().forEach { index, value in
+    if value ?? Int.max < min, value != nil {
         min = value ?? Int.max
         i = index
     }
-} )
-print("Minimal int takes \(i+1) place and equals \(min)")
+}
+
+print("Minimal int takes \(i + 1) place and equals \(min)")
