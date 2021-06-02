e135bbe35b243aaac417e26bb26d61336355d750
rndmking <31206554+rndmking@users.noreply.github.com>
2021-06-02 20:00:38 +0300

    handling emojis and int.max

--- main.swift
+++ main.swift
@@ -7,16 +7,17 @@
 
 import Foundation
 
-var i : Int = 1
+var i: Int = 1
 var input = readLine() ?? ""
-var array2: [String] = input.components(separatedBy: " ")
-var array: [Int]  = array2.compactMap{Int($0)}
+// var array5: String = input.unicodeScalars.filter { !$0.properties.isEmojiPresentation }.reduce(""){$0  + String($1)}
+var array4: [String] = input.components(separatedBy: " ")
+var array: [Int?] = array4.map {Int($0)}
 var min: Int = Int.max
 var index: Int
 i = 1
 array.enumerated().forEach( {index, value in
-    if value < min {
-    min = value
+    if value ?? Int.max < min && value != nil {
+        min = value ?? Int.max
         i = index
     }
 } )
